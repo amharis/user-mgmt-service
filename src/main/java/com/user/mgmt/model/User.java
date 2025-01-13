78ec14365484e8a6e327c286f502b3e0f949ffc1
Haris Abdullah <haris.abdullah@vaisala.com>
2025-01-13 22:51:20 +0200

    adding user model

--- src/main/java/com/user/mgmt/model/User.java
+++ src/main/java/com/user/mgmt/model/User.java
@@ -1,4 +1,32 @@
 package com.user.mgmt.model;
 
 public class User {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
+    public User() {}
+
+    public User(String id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+
 }
